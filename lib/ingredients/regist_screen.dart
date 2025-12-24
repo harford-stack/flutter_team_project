70f6ade8c73611c028edc0f600a74390ac1fbcb4
html_learn <duskfex@gmail.com>
2025-12-24 17:57:43 +0900

    2025.12.24 ingredients

---
+++ lib/ingredients/regist_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const regist());
+}
+
+class regist extends StatelessWidget {
+  const regist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+
+              ),
+
+              Container()
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
